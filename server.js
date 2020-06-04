b0d3e1e15421f9ce542cf724ee44c9e8f5646697
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2020-06-04 15:26:52 -0600

    trying to fix Heroku

--- server.js
+++ server.js
@@ -44,8 +44,9 @@ mongoose.connect(
     process.env.MONGODB_URI ||
     "mongodb://"+process.env.DB_USER+":"+process.env.DB_PASS+"@ds031995.mlab.com:31995/heroku_q3cw6k5t",
     {
-        useNewURLParser: true
-    }
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
 );
 
 // Routes
